df5fa23c7c887c2c89308eaba74ac57ae4ec9523
Rose <roshanjeyarubanr@gmail.com>
2024-10-25 12:00:14 +0530

    summa commit

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-console.log("vali mamey valipu");
-
-
+const express = require('express')
+const app = express()
+app.get("/",(req,res)=>{
+    if (pass="ninja") {
+        res.send("ull akonjo madaki thattu")
+    }
+})
