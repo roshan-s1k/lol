dfbc2ee887d1271c18c2932e0b2250ed998ed5c2
Rose <roshanjeyarubanr@gmail.com>
2024-10-25 12:27:20 +0530

    optimized code

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
 app.get("/",(req,res)=>{
-    if (pass="ninja") {
-        res.send("ull akonjo madaki thattu")
+    const pass = req.query.pass;
+    if (pass!="ninja") {
+        res.status(400).json({msg :"ull akonjo madaki thattu"})
+        return
     }
+    res.send("succesfully logged in")
+    
 })
+app.listen(3000)
